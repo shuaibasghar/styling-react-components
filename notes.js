f42642907f919170686b3d13b9cd030ade27083a
shuaibasghar <shuaibasghargtk@gmail.com>
2024-06-10 17:32:28 +0500

    some css basic notes

---
+++ notes.js
@@ -0,0 +1,10 @@
+// if you write styling for p-tag in header.css it will also be apllied globally to all p-tags in the project
+
+// you can also use inline css style={{color:'red',}}
+
+//conditionally add inline style dynamically
+// style={{backgroundColor: emailNotValid ? 'red' : 'white'}}
+
+// you can also use css modules
+// import styles from './Header.module.css';
+// it has file -scoped css  if you apply style p-tag in header.module.css it will only be applied to p-tag in header.jsx
